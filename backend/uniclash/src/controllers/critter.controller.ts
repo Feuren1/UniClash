615bf5e1cdcf70e34617d73989a1e48cb215bdc2
Matthis van Treeck <matthis.vantreeck@student.fontys.nl>
2023-11-01 16:06:19 +0100

    add crittercontroller

---
+++ backend/uniclash/src/controllers/critter.controller.ts
@@ -0,0 +1,150 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getModelSchemaRef,
+  patch,
+  put,
+  del,
+  requestBody,
+  response,
+} from '@loopback/rest';
+import {Critter} from '../models';
+import {CritterRepository} from '../repositories';
+
+export class CritterController {
+  constructor(
+    @repository(CritterRepository)
+    public critterRepository : CritterRepository,
+  ) {}
+
+  @post('/critters')
+  @response(200, {
+    description: 'Critter model instance',
+    content: {'application/json': {schema: getModelSchemaRef(Critter)}},
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Critter, {
+            title: 'NewCritter',
+            exclude: ['id'],
+          }),
+        },
+      },
+    })
+    critter: Omit<Critter, 'id'>,
+  ): Promise<Critter> {
+    return this.critterRepository.create(critter);
+  }
+
+  @get('/critters/count')
+  @response(200, {
+    description: 'Critter model count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async count(
+    @param.where(Critter) where?: Where<Critter>,
+  ): Promise<Count> {
+    return this.critterRepository.count(where);
+  }
+
+  @get('/critters')
+  @response(200, {
+    description: 'Array of Critter model instances',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(Critter, {includeRelations: true}),
+        },
+      },
+    },
+  })
+  async find(
+    @param.filter(Critter) filter?: Filter<Critter>,
+  ): Promise<Critter[]> {
+    return this.critterRepository.find(filter);
+  }
+
+  @patch('/critters')
+  @response(200, {
+    description: 'Critter PATCH success count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async updateAll(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Critter, {partial: true}),
+        },
+      },
+    })
+    critter: Critter,
+    @param.where(Critter) where?: Where<Critter>,
+  ): Promise<Count> {
+    return this.critterRepository.updateAll(critter, where);
+  }
+
+  @get('/critters/{id}')
+  @response(200, {
+    description: 'Critter model instance',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(Critter, {includeRelations: true}),
+      },
+    },
+  })
+  async findById(
+    @param.path.number('id') id: number,
+    @param.filter(Critter, {exclude: 'where'}) filter?: FilterExcludingWhere<Critter>
+  ): Promise<Critter> {
+    return this.critterRepository.findById(id, filter);
+  }
+
+  @patch('/critters/{id}')
+  @response(204, {
+    description: 'Critter PATCH success',
+  })
+  async updateById(
+    @param.path.number('id') id: number,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Critter, {partial: true}),
+        },
+      },
+    })
+    critter: Critter,
+  ): Promise<void> {
+    await this.critterRepository.updateById(id, critter);
+  }
+
+  @put('/critters/{id}')
+  @response(204, {
+    description: 'Critter PUT success',
+  })
+  async replaceById(
+    @param.path.number('id') id: number,
+    @requestBody() critter: Critter,
+  ): Promise<void> {
+    await this.critterRepository.replaceById(id, critter);
+  }
+
+  @del('/critters/{id}')
+  @response(204, {
+    description: 'Critter DELETE success',
+  })
+  async deleteById(@param.path.number('id') id: number): Promise<void> {
+    await this.critterRepository.deleteById(id);
+  }
+}
--- backend/uniclash/src/controllers/index.ts
+++ backend/uniclash/src/controllers/index.ts
@@ -4,3 +4,4 @@ export * from './critter-critter-copy.controller';
 export * from './critter-copy-critter.controller';
 export * from './item-item-copy.controller';
 export * from './item-copy-item.controller';
+export * from './critter.controller';

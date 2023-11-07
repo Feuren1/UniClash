f6ad7b2ea5e377499668544143acd33afa1b9699
Matthis van Treeck <matthis.vantreeck@student.fontys.nl>
2023-11-07 16:40:46 +0100

    fix: removed dependencies from wildcounter

--- backend/uniclash/src/models/index.ts
+++ backend/uniclash/src/models/index.ts
@@ -1,14 +1,14 @@
 
-export * from './trainer.model';
-export * from './critter.model';
-export * from './critter-copy.model';
-export * from './item.model';
-export * from './item-copy.model';
-export * from './attack.model';
-export * from './type.model';
 export * from './arena.model';
-export * from './student-hub.model';
-export * from './wild-encounter.model';
-export * from './user.model';
-export * from './user-credentials.model';
+export * from './attack.model';
+export * from './critter-copy.model';
+export * from './critter.model';
+export * from './item-copy.model';
+export * from './item.model';
 export * from './refresh-token.model';
+export * from './student-hub.model';
+export * from './trainer.model';
+export * from './type.model';
+export * from './user-credentials.model';
+export * from './user.model';
+

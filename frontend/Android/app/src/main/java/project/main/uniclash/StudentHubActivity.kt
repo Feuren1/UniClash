9c050a80bc215ae39cf3099e390109ba58261de6
Raphael-Kl <517878@student.fontys.nl>
2023-11-15 11:39:53 +0100

    Update: StudentHub basic GUI
    - created the basic structure of the StudentHub GUI.
    - there is a problem with the retrieval of data from the DB, so I made a small placeholder list to test the GUI.
    - Button does nothing, will need to figure out how to actually buy one and the connection with the player's inventory etc.

--- frontend/Android/app/src/main/java/project/main/uniclash/StudentHubActivity.kt
+++ frontend/Android/app/src/main/java/project/main/uniclash/StudentHubActivity.kt
@@ -7,14 +7,18 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import project.main.uniclash.datatypes.Item
 import project.main.uniclash.retrofit.StudentHubService
 import project.main.uniclash.ui.theme.UniClashTheme
 import project.main.uniclash.viewmodels.StudentHubViewModel
@@ -43,28 +47,61 @@ class StudentHubActivity : ComponentActivity() {
 @Composable
 fun StudentHub(modifier: Modifier = Modifier, studentHubViewModel: StudentHubViewModel) {
 
-    val studentHubState by studentHubViewModel.studentHub.collectAsState()
-    studentHubViewModel.loadStudentHub(1)
+//    val studentHubState by studentHubViewModel.studentHub.collectAsState()
+//    studentHubViewModel.loadStudentHub(1)
+//
+//    val itemsHubState by studentHubViewModel.items.collectAsState()
+//    studentHubViewModel.loadItems()
+//
+//    var studentHub = studentHubState.studentHub
+//    var itemList = itemsHubState.items
 
-    val itemsHubState by studentHubViewModel.items.collectAsState()
-    studentHubViewModel.loadItems()
+    var itemList = List(2) { i -> Item("Item$i", 5) }
 
-    var studentHub = studentHubState.studentHub
-    var itemList = itemsHubState.items
+    println("Items: $itemList")
 
-    Column() {
+    Column(modifier = modifier) {
 
-        Button(onClick = {
-            println("StudentHub: $studentHub")
-            println("Items: $itemList")
-        } ) {
+        
 
-            Text(text = "Debug")
+//        Button(onClick = {
+//            println("Items: $itemList")
+//            println("StudentHub: $studentHub")}) {
+//            Text(text = "Debug")
+//        }
+
+        ItemList(itemList, onButtonClicked = {/*TODO: Buying the item*/})
+    }
+}
+
+@Composable
+fun ItemList(itemList: List<Item>, onButtonClicked: () -> Unit, modifier: Modifier = Modifier) {
+
+    LazyColumn(modifier = modifier) {
+
+        items(items = itemList, key = {item -> item.name}) {
+
+            item -> ItemRow(itemName = item.name, itemCost = item.cost, onButtonClicked = {/*TODO: Buying the item*/})
         }
+    }
+}
 
-        Row() {
+@Composable
+fun ItemRow(itemName: String, itemCost: Int, onButtonClicked: () -> Unit, modifier: Modifier = Modifier) {
 
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
 
+        Text(modifier = Modifier
+            .weight(1f)
+            .padding(start = 16.dp),
+            text = "Item: $itemName = Cost: $itemCost")
+        
+        Button(modifier = Modifier
+            .weight(1f)
+            .padding(end = 16.dp),
+            onClick = onButtonClicked) {
+            
+            Text(text = "Buy")
         }
     }
 }

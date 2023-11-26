800fddc6ef72391058e1c980842dbc43cfaafc66
Elias <520268@student.fontys.nl>
2023-11-26 01:57:07 +0100

    .

--- frontend/Android/app/src/main/java/project/main/uniclash/WildEncounterActivity.kt
+++ frontend/Android/app/src/main/java/project/main/uniclash/WildEncounterActivity.kt
@@ -1,24 +1,17 @@
 package project.main.uniclash
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,21 +20,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import project.main.uniclash.datatypes.CritterPic
 import project.main.uniclash.datatypes.MapSaver
-import project.main.uniclash.datatypes.MapSettings
-import project.main.uniclash.datatypes.SelectedMarker
 import project.main.uniclash.retrofit.CritterService
-import project.main.uniclash.ui.theme.UniClashTheme
-import project.main.uniclash.viewmodels.BattleViewModel
 import project.main.uniclash.viewmodels.WildEncounterViewModel
 
 
@@ -109,7 +95,10 @@ class WildEncounterActivity : ComponentActivity() {
                 this.startActivity(intent)
             }
             if(catchCritter){
-                wildEncounterActivity.addWildEncounterToUser()
+                val preferences = this.getSharedPreferences("Ids", Context.MODE_PRIVATE)
+                val studentId = preferences.getInt("StudentId", 1) ?: 1
+
+                wildEncounterActivity.addWildEncounterToUser(studentId)
                 val intent = Intent(this, MapActivity::class.java)
                 this.startActivity(intent)
             }
--- frontend/Android/app/src/main/java/project/main/uniclash/viewmodels/WildEncounterViewModel.kt
+++ frontend/Android/app/src/main/java/project/main/uniclash/viewmodels/WildEncounterViewModel.kt
@@ -1,25 +1,18 @@
 package project.main.uniclash.viewmodels
 
 import android.content.ContentValues.TAG
-import android.content.Intent
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.flow.update
-import project.main.uniclash.MapActivity
-import project.main.uniclash.WildEncounterActivity
-import project.main.uniclash.datatypes.CritterForStudent
 import project.main.uniclash.datatypes.CritterUsable
 import project.main.uniclash.datatypes.MyMarker
 import project.main.uniclash.datatypes.SelectedMarker
 import project.main.uniclash.retrofit.CritterService
 import project.main.uniclash.retrofit.enqueue
-import retrofit2.Call
 
 sealed interface PostCrittersUIState {
     data class HasEntries(
@@ -47,11 +40,12 @@ class WildEncounterViewModel(
         )
     )
 
-    fun addWildEncounterToUser(){
+    fun addWildEncounterToUser(studentId: Int) {
+
         viewModelScope.launch {
             critters.update { it.copy(isLoading = true) }
             try {
-                val response = critterService.postCatchedCritter(1,
+                val response = critterService.postCatchedCritter(studentId,
                     wildEncounterMarker!!.critterUsable!!.critterId).enqueue()
                 Log.d(TAG, "loadCrittersUsable: $response")
                 if (response.isSuccessful) {

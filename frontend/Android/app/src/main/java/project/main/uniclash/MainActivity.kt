b18f2596b26e3a4618ab4b75a382af571c808472
Elias <520268@student.fontys.nl>
2023-12-10 15:38:52 +0100

    :fix main activity with 3 buttons

--- frontend/Android/app/src/main/java/project/main/uniclash/MainActivity.kt
+++ frontend/Android/app/src/main/java/project/main/uniclash/MainActivity.kt
@@ -1,5 +1,6 @@
 package project.main.uniclash
 
+import android.app.Application
 import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
@@ -8,12 +9,21 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,12 +35,19 @@ import androidx.datastore.preferences.preferencesDataStore
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
+import project.main.uniclash.retrofit.UserService
+import project.main.uniclash.viewmodels.ProfileViewModel
+
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "UserData")
 class MainActivity : ComponentActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val profileViewModel: ProfileViewModel by viewModels(factoryProducer = {
+            ProfileViewModel.provideFactory(UserService.getInstance(this), Application())
+        })
         super.onCreate(savedInstanceState)
+        var activityContext = this
 
         FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
             if (task.isSuccessful) {
@@ -50,27 +67,77 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
-                OpenLoginActivity()
-                OpenProfileActivity()
+                val preferences = this.getSharedPreferences("Ids", Context.MODE_PRIVATE)
+                val token = preferences.getString("UserId", "") ?: ""
+                Column(
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    OpenLoginActivityButton()
+                    Spacer(modifier = Modifier.height(8.dp))
+                    OpenRegisterActivityButton()
+                    if (profileViewModel.loadProfile(token, activityContext) != null) {
+                        Spacer(modifier = Modifier.height(8.dp))
+                        OpenProfileActivityButton()
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
             }
         }
     }
 
 
     @Composable
-    fun OpenLoginActivity() {
+    fun OpenLoginActivityButton() {
         val context = LocalContext.current
-                val intent = Intent(context, LoginActivity::class.java)
-                this.startActivity(intent)
+        Button(
+           onClick = {
+        val intent = Intent(context, LoginActivity::class.java)
+        this.startActivity(intent)
+          },
+           modifier = Modifier
+               .padding(2.dp)
+               .fillMaxWidth()
+               .height(50.dp)
 
+         ) {
+           Text("Log in")
+         }
     }
 
     @Composable
-    fun OpenProfileActivity() {
+    fun OpenRegisterActivityButton() {
         val context = LocalContext.current
-        val intent = Intent(context, LoginActivity::class.java)
-        this.startActivity(intent)
+        Button(
+            onClick = {
+                val intent = Intent(context, LoginActivity::class.java)
+                this.startActivity(intent)
+            },
+            modifier = Modifier
+                .padding(2.dp)
+                .fillMaxWidth()
+                .height(50.dp)
 
+        ) {
+            Text("Register")
+        }
+    }
+
+    @Composable
+    fun OpenProfileActivityButton() {
+        val context = LocalContext.current
+        Button(
+            onClick = {
+                val intent = Intent(context, ProfileActivity::class.java)
+                this.startActivity(intent)
+            },
+            modifier = Modifier
+                .padding(2.dp)
+                .fillMaxWidth()
+                .height(50.dp)
+
+        ) {
+            Text("Go to old session")
+        }
     }
     // typically you want to retrieve the device token when the user logs in and save
 // it in the backend when the login is success
